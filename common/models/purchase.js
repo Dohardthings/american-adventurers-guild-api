c599431d0ec8dcdbd08d0ec2eecf54379626d5fa
Daniel McBryde <mcbryded@gmail.com>
2016-05-17 10:21:28 -0500

    add js to save date on purchase

--- common/models/purchase.js
+++ common/models/purchase.js
@@ -1,3 +1,12 @@
 module.exports = function(Purchase) {
+  Purchase.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
 
+    req.body.data.attributes['date'] = Date.now();
+    req.body.data.relationships['end-user'].data = {
+      id: req.accessToken.userId,
+    };
+
+    next();
+  });
 };
